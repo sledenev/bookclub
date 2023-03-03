48c1105b977cd3ced4c597f2125ac19ed19e2524
ledenevs <sergei.ledenev@adsum-works.com>
2023-03-03 10:54:09 +0000

    add listing

---
+++ clean_code/chapter_7_error_handling/7_1_error_codes.py
@@ -0,0 +1,59 @@
+import logging
+from enum import Enum, auto
+
+
+class Device:
+    id: int
+    pass
+
+
+class DeviceHandle(Enum):
+    INVALID = auto()
+
+
+class DeviceStatus(Enum):
+    DEVICE_SUSPENDED = auto()
+
+
+class DeviceRecord(object):
+    def get_status(self) -> DeviceStatus:
+        pass
+
+
+class DeviceController(object):
+    record: DeviceRecord
+
+    def get_handle(self, device_id: int) -> DeviceHandle:
+        pass
+
+    def send_shut_down(self, device: Device) -> None:
+        handle: DeviceHandle = self.get_handle(device.id)
+
+        # Check the state of the device
+        if handle != DeviceHandle.INVALID:
+            # Save the device status to the record field 
+            self.retrieve_device_record(handle)
+            # If not suspended, shut down
+            if self.record.get_status() != DeviceStatus.DEVICE_SUSPENDED:
+                self.pause_device(handle)
+                self.clear_device_work_queue(handle)
+                self.close_device(handle)
+            else:
+                logging.log("Device suspended. Unable to shut down")
+        else:
+            logging.log("Invalid handle for: " + str(device))
+
+    def retrieve_device_record(self, handle):
+        pass
+
+    def shut_down(self, handle):
+        pass
+
+    def pause_device(self, handle):
+        pass
+
+    def clear_device_work_queue(self, handle):
+        pass
+
+    def close_device(self, handle):
+        pass
